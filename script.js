7946474c6e576eeef5732fd0514df06ff8fcaff6
Imaan Yusuf <imaanyusuf@surfer-172-29-6-193-www.highspeed-internet-access.com>
2023-01-10 16:02:05 +0000

    work ongoing

---
+++ script.js
@@ -0,0 +1,62 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 9);
+
+console.log("jibril")
+
+
+let firstName = "mohammed";
+console.log(firstName)
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let myLastJob = "hayes group";
+console.log(myLastJob);
+*/
+
+
+let javaScripIsFun = true;
+console.log(javaScripIsFun);
+
+console.log(typeof true)
+console.log(typeof javaScripIsFun);
+console.log(typeof 23);
+console.log(typeof "jibril")
+
+
+javaScripIsFun = "essensial";
+console.log(typeof javaScripIsFun);
+
+
+
+let age = 30;
+age = 77;
+
+console.log(age)
+
+
+const time = 17.7;
+const birthYear = 1991;
+console.log(time, birthYear)
+
+
+
+
+const now = 2037;
+const ageJibril = now - 1991;
+const ageMohamed = now - 2018;
+
+console.log(ageJibril * 2, ageMohamed / 2);
+
+const obj = {};
+
+obj.x = 3;
+console.log(obj.x); // Prints 3.
+console.log(obj); // Prints { x: 3 }.
+
+const key = "y";
+obj[key] = 5;
+console.log(obj[key]); // Prints 5.
+console.log(obj); // Prints { x: 3, y: 5 }.
